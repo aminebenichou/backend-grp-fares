73ab684c737d2af76ddd295a10d5a119899398cd
aminebenichou <aamine060@gmail.com>
2026-03-14 12:46:53 +0100

    sync

--- twitter_backend/posts/views.py
+++ twitter_backend/posts/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from rest_framework import viewsets, generics, authentication
+from rest_framework import viewsets, generics, authentication, response, status
 from .models import Post
 from .serializers import *
 from users.permissions import isGmailUser, canAddPost
+from rest_framework.decorators import action
 # Create your views here.
 
 
@@ -10,7 +11,18 @@ class PostsViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializerGeneral
     authentication_classes =[authentication.BasicAuthentication]
-    permission_classes = [isGmailUser, canAddPost]
+    # permission_classes = [isGmailUser, canAddPost]
+
+    @action(detail=True, methods=['get'])
+    # if detail is true that means django expects id in url
+    # url format is http://127.0.0.1:8000/api/posts/posts/{id}/{method-name}/
+    def like_post(self, request, pk=None):
+        post = self.queryset.get(id=pk)
+        data ={
+            'post_content': post.content,
+            'liked':False
+        }
+        return response.Response(data, status=status.HTTP_200_OK)
 
 
 class PostsGenericView(generics.ListCreateAPIView):
